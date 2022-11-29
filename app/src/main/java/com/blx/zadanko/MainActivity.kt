14dfc82b8901fc3283bfa9c19ac9179adbee0036
sztoslol <kubuszeski05@wp.pl>
2022-11-29 10:09:43 +0100

    Initial commit

---
+++ app/src/main/java/com/blx/zadanko/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.blx.zadanko
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
